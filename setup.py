8a19aab2deecf12768222526cd1f2e17cd260b30
Taiza <taizarabello@gmail.com>
2017-05-09 11:16:53 -0300

    Renaming from readme.txt to readme.md

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
 requires = [
     'pyramid',
@@ -25,7 +23,7 @@ setup(
     name='parser_feed_to_json_pyramid',
     version='0.0',
     description='parser-feed-to-json-pyramid',
-    long_description=README + '\n\n' + CHANGES,
+    long_description=README,
     classifiers=[
         'Programming Language :: Python',
         'Framework :: Pyramid',
